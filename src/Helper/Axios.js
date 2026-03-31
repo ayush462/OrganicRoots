2b65ae493e248f4692fb28d8b4b6ab0151da7638
Ayush kumar <84299616+ayush462@users.noreply.github.com>
2026-03-31 17:14:09 +0530

    timeout fixed

--- src/Helper/Axios.js
+++ src/Helper/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://organicrootsbackend.onrender.com/",
-  timeout: 15000,
+  timeout: 60000,
 });
 
 // 🔐 Request interceptor (attach token)
